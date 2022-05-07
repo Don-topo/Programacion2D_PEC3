d3ad018ad9f3c5f84b8f2543189199912eca9396
Rubén Garrido <rgarridovaldepenas@uoc.edu>
2022-05-07 20:54:45 +0200

    Add key interface to chest interaction

--- PEC2_Un-Juego-De-Plataformas/Assets/Scripts/SpawnCoins.cs
+++ PEC2_Un-Juego-De-Plataformas/Assets/Scripts/SpawnCoins.cs
@@ -11,6 +11,14 @@ public class SpawnCoins : MonoBehaviour
     public float minSpreadRange = 1.0f;
     public float maxSpreadRange = 1.5f;
     public bool open = false;
+    public GameObject helpGameObject;
+
+    private BoxCollider2D chestCollider;
+
+    private void Awake()
+    {        
+        chestCollider = gameObject.GetComponent<BoxCollider2D>();
+    }
 
     private void Update()
     {
@@ -31,6 +39,23 @@ public class SpawnCoins : MonoBehaviour
             var yForce = Random.Range(5f, 7f);
             Vector2 force = new Vector2(xForce, yForce);
             coin.GetComponent<Rigidbody2D>().AddRelativeForce(force);
+            chestCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            helpGameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            helpGameObject.SetActive(false);
         }
     }
 
